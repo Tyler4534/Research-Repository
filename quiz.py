00b5e02e9a8599620a42c9573c747dae97ee3676
Tyler4534 <111465373+Tyler4534@users.noreply.github.com>
2022-09-01 15:23:37 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -24,15 +24,15 @@ else:
   user = input('\n Good bye '   +  name  +  ' have a nice day! ')
   
   
-# From yes program continues here answer_(num) input is asking for user input to answer the question   (Line 28 repeats on line: 39,47,55,63,71,77)
+# From yes program continues here answer_(num) input is asking for user input to answer the question   (Line 28 repeats on line: 39,46,53,60,69,75)
 answer_1 = input("\n Q1 - How many countries are their in the world: ")
-# If the user gets answer_(num)  answer correct it will be equal to the correct answer example for answer_1 == "195": (Line 30 repeats on line: 40,48,56,64,72,78)
+# If the user gets answer_(num)  answer correct it will be equal to the correct answer example for answer_1 == "195": (Line 30 repeats on line: 40,47,54,61,70,76)
 if answer_1 == "195":
-  # If user types the correct answer it'll print out a message saying that they're correct. (Line 32 repeats on line: 41,49,57,65,73,79)
+  # If user types the correct answer it'll print out a message saying that they're correct. (Line 32 repeats on line: 41,48,55,62,71,77)
   print("You're correct, There are 195 countries! ")
-# If answer isn't equal to correct answer else statement would be used to print its an incorrect answer to the question (Line 34 repeats on line:42,50,58,66,74,80)
+# If answer isn't equal to correct answer else statement would be used to print its an incorrect answer to the question (Line 34 repeats on line:42,49,56,63,72,78)
 else:
- # If user's answer incorrect it will print that it is incorrect and give them the right answer. (Line 36 repeats on line: 43,51,59,67,76,82
+ # If user's answer incorrect it will print that it is incorrect and give them the right answer. (Line 36 repeats on line: 43,50,57,64,73,79
   print(f"Incorrect! The answer is 195, not {answer_1!r}")
   
 
@@ -43,7 +43,6 @@ else:
     print(f"Incorrect! The answer is 7, not {answer_2!r} ")
 
   
-
 answer_3 = input("\n Q3 - What is the largest country in the world: ")
 if answer_3 == "russia":
   print("You're correct! Russia is the largest country in the world. ")
@@ -51,7 +50,6 @@ else:
   print(f"You're Incorrect! Russia is the largest country in the world, not {answer_3!r}")
 
   
-
 answer_4 = input("\n Q4 - Which country has the largest population in the world: ")
 if answer_4 == "china":
   print("You're correct! China has the largest population in the world. ")
@@ -59,7 +57,6 @@ else:
   print(f"You're incorrect! China has the largest population in the world, not {answer_4!r}")
 
   
-
 answer_5 = input("\n Q5 -  What is the hottest continent in the world: ")
 if answer_5 == "africa":
   print("You're correct! Africa is the hottest continent. ")
